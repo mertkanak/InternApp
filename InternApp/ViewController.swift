24d61b08a14230e0dbcd34e60d18e838db1f03e2
Mert Kanak <107624227+mertkanak@users.noreply.github.com>
2023-08-09 21:30:34 +0300

    Initial Commit

---
+++ InternApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternApp
+//
+//  Created by mert Kanak on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
